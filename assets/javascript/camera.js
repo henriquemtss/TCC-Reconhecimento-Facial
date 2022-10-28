60391b4ba4f268069fd7660e4a50a3428750d670
VictorLagunaR <laguna.vitorc@gmail.com>
2022-10-27 23:26:15 -0300

    adicionando verificação no login via javascript

--- assets/javascript/camera.js
+++ assets/javascript/camera.js
@@ -1,33 +1,21 @@
-var click = 0;
 function loadCamera(){
-	click++;
 	//Captura elemento de vídeo
 	var video = document.querySelector("#webCamera");
-	
-	//As opções abaixo são necessárias para o funcionamento correto no iOS
-	video.setAttribute('autoplay', '');
-	video.setAttribute('muted', '');
-	video.setAttribute('playsinline', '');
-	//--
+		//As opções abaixo são necessárias para o funcionamento correto no iOS
+		video.setAttribute('autoplay', '');
+	    video.setAttribute('muted', '');
+	    video.setAttribute('playsinline', '');
+	    //--
 	
 	//Verifica se o navegador pode capturar mídia
-	if (click == 1 && navigator.mediaDevices.getUserMedia) {
+	if (navigator.mediaDevices.getUserMedia) {
 		navigator.mediaDevices.getUserMedia({audio: false, video: {facingMode: 'user'}})
 		.then( function(stream) {
 			//Definir o elemento víde a carregar o capturado pela webcam
 			video.srcObject = stream;
-			//if (document.getElementById('webCamera').style.display == 'none') {
-				document.getElementById('webCamera').style.display = 'block';
-				document.getElementById('noCamera').style.display = 'none';
-				document.getElementById('reconhecer').style.display = 'none';
-				document.getElementById('capturar').style.display = 'block';
-			// } else {
-			// 	document.getElementById('webCamera').style.display = 'none';
-			// 	document.getElementById('noCamera').style.display = 'block';
-			// }
 		})
 		.catch(function(error) {
-			alert("Não Será Possível Capturar Imagem, Recarregue a página e Tente Novamente. Caso o erro persista, contate o administrador!");
+			alert("Oooopps... Falhou :'(");
 		});
 	}
 }
@@ -85,5 +73,7 @@ function sendSnapShot(base64){
 		request.send("base_img="+base64); // Enviar dados
 }
 
+
+loadCamera();
 
                 
