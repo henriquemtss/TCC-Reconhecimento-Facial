235c35b6aa3a6cffec078508dea50e36a2dd63db
WesleyMR1 <WesleyMR012@gmail.com>
2022-11-01 20:19:44 -0300

    atualizando sql

--- extra/faceid.sql
+++ extra/faceid.sql
@@ -14,10 +14,18 @@ create table  Contas(
 
 insert into Contas values(0,'admin', 'admin', '$2y$10$hxb5L22B/FJmZyLx.MM8JuJ8v7vXI1GjTMRva3LGgRdu5Ws8DtDo2');
 
-create table Cadastro(
+create table CadastroAlunos(
     nome varchar(140) not null,
     rm int(5) not null primary key check (rm > 0 and rm <= 99999 ),
     email varchar(100) not null unique key,
     curso varchar(7) not null,
     periodo varchar(10) not null
+);
+
+create table CadastroFuncionarios(
+    nome varchar(140) not null,
+    CPF varchar(14) not null primary key,
+    email varchar(100) not null unique key,
+    funcao varchar(7) not null,
+    telefone varchar(15) not null
 );
