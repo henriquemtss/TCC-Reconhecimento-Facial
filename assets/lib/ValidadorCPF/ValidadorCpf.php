a6fd72d319a964fd0565b8938e0000e70b3f6a63
WesleyMR1 <WesleyMR012@gmail.com>
2022-11-14 10:04:35 -0300

    ValidadorCPF corrigido

--- assets/lib/ValidadorCPF/ValidadorCpf.php
+++ assets/lib/ValidadorCPF/ValidadorCpf.php
@@ -1,84 +1,49 @@
 <?php
-
 function ValidadorCPF($numeroCPF){
 
-        $cpf = preg_replace("/[^0-9]/", "", $numeroCPF);
-    $cpf2 = str_split($cpf);
-    $array[] = [11,10,9,8,7,6,5,4,3,2];
-    $temp = [];
-    $soma = 0;
-    $v1 = 0;
+    $cpf = str_split(preg_replace("/[^0-9]/", "", $numeroCPF));
+    $v1 = 0; 
     $v2 = 0;
-    $d1=0;
-    $d2=0;
-
-    //1 - pega os numeros e coloca no array
-    for ($i=0; $i < 11; $i++) { 
-        array_push($temp, intval($cpf2[$i]));
-    }
-    echo "<br>";
-    //faz a matriz
-    array_push($array, $temp);
-    $temp = [];
-    $v1 = $array[1][9] ;
-    $v2 = $array[1][10];
-
-    // 2 - multiplicação da matriz
-    for ($i=0; $i < 9; $i++) { 
-        array_push($temp, $array[0][$i+1] * $array[1][$i]);
-    }
-    //
-    array_push($array, $temp);
-    $temp = [];
-
-    // 3 - 
-    for ($i=0; $i < 9; $i++) { 
-        $soma += intval($array[2][$i]);
-    }
-    $soma %= 11;
-    
-    //
-    if (($soma) > 2) {
-        $d1 = 11 - $soma;
-    }else {
-        $d1 = 0;
-    }
-
-    ////////////
     $soma = 0;
-    
-    $array[1][9] = $v1;
+    $d1=intval($cpf[9]); 
+    $d2=intval($cpf[10]);
 
-    for ($i=0; $i < 10; $i++) { 
-        array_push($temp, $array[0][$i] * $array[1][$i]);
-    }
-    //
-    array_push($array, $temp);
-    $temp = [];
-
-    for ($i=0; $i < 10; $i++) { 
-        $soma += $array[3][$i];
+    //Verificando o Primeiro Digito
+    $x = 10;
+    for ($i=0; $i < 9; $i++) { 
+        $v1 = intval($cpf[$i]) * $x;
+        $x--;
+        $soma += $v1;
     }
     $soma %= 11;
-
-    if ($soma > 2) {
-        $d2 = 11 - $soma;
-    }else {
-        $d2 = 0;
+    if ($soma >= 2) {
+        $v1 = 11 - $soma;
+    } else {
+        $v1 = 0;
     }
 
-    ///////////////
-
+    //Verificando o Segundo Digito
+    $x = 11;
+    $soma = 0;
+    for ($i=0; $i < 10; $i++) { 
+        $v2= intval($cpf[$i]) * $x;
+        $x--;
+        $soma += $v2;
+    }
+    $soma %= 11;
+    if ($soma >= 2) {
+        $v2 = 11 - $soma;
+    } else {
+        $v2 = 0;
+    }
+    //Resultado
     if (($v1 == $d1) && ($v2 == $d2)){
         return true;
     } else {
         return false;
     }
-
-
-
-
-    }
+}
+?>
 
     
 
