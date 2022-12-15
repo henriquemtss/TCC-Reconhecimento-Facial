200adf0aa8cb71bfe93f9a1717701b4ab9587d87
henriquemtss <henriquegomesdematos08@gmail.com>
2022-12-15 12:46:06 -0300

    Update splash tela cadastro

--- assets/javascript/splash.js
+++ assets/javascript/splash.js
@@ -5,53 +5,26 @@ let logoSapan = document.querySelectorAll('.logo-parts');
 window.addEventListener('DOMContentLoaded', () => {
 
    setTimeout(() => {
-    logoSapan.forEach((span, index) => {
+        logoSapan.forEach((span, index) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (index + 1) *100);
+        });
+
         setTimeout(() => {
-            span.classList.add('active');
-        }, (index + 1) *100);
-    });
-
-    setTimeout(() => {
-        logoSapan.forEach((span, index) =>{
-            setTimeout(() => {
-                span.classList.remove('active');
-                span.classList.add('inactive');
-            }, (index + 1) * 100);
-        });
-    }, 3000);
-
-    setTimeout(() => {
-        logoSapan.forEach((span, index) =>{
-            setTimeout(() => {
-                span.classList.remove('inactive');
-                span.classList.add('active');
-            }, (index + 1) * 100);
-        });
-    }, 6000);
-
-    setTimeout(() => {
-        logoSapan.forEach((span, index) =>{
-            setTimeout(() => {
-                span.classList.remove('active');
-                span.classList.add('fade');
-            }, (index + 1) * 100);
-        });
-    }, 9000);
-
-    setTimeout(() => {
-        logoSapan.forEach((span, index) =>{
-            setTimeout(() => {
-                span.classList.remove('fade');
-                span.classList.add('active');
-            }, (index + 1) * 100);
-        });
-    }, 12000);
+            logoSapan.forEach((span, index) =>{
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (span + 1) * 50);
+            });
+        }, 2000);
         
         setTimeout(() => {
 
             intro.style.top = '-100vh';
             
-        }, 20000);
+        }, 2300);
 
    });
 
