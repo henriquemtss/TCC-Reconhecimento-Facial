0529ab0fd3aba17161d4df8b404c420d685bd756
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-12-17 18:07:23 -0300

    Fixed Multiplos Canvas Apos Registro Manual

--- assets/javascript/registrar.js
+++ assets/javascript/registrar.js
@@ -1,8 +1,6 @@
 function register() {
   var time =null;
-  document.getElementById('cam').pause();
-  document.getElementById('cam').src = "";
-  document.getElementById('cam').srcObject.getTracks()[0].stop();
+  document.getElementById('cam').srcObject.getTracks()[0].enabled = false;
   console.log("Vid off");
   time = setTimeout(function(){
     document.getElementById('rm').style.display = 'none';
@@ -30,34 +28,17 @@ if (API === 'api') {
   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
   request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
-      console.log(request.responseText);
+    console.log(request.responseText);
         if (JSON.parse(request.responseText) === false && API === 'sendRm') {
           document.getElementById('rm').style.display = 'block';
           document.getElementById('nome').style.display = 'block';
           document.getElementById('periodo').style.display = 'block';
           document.getElementById('curso').style.display = 'block';
           document.getElementById('rmMan').style.display = 'none';
+          document.getElementById('rmMan').value = "";
           document.getElementById('manual').style.display = 'block';
           document.getElementById('sendRm').style.display = 'none';
-          navigator.mediaDevices.enumerateDevices()
-          .then(devices => {
-              console.log(devices)
-              if (Array.isArray(devices)) {
-                 
-                  devices.forEach(device => {
-                      if(device.kind == 'videoinput'){
-                          console.log(device);                        
-                          navigator.getUserMedia(
-                              {video: {
-                                  deviceId: device.deviceId,
-                              }},
-                              stream => cam.srcObject = stream,
-                              error => console.log(error)
-                          )
-                      }
-                  })
-              }
-          })
+          document.getElementById('cam').srcObject.getTracks()[0].enabled = true;
           alert("Registrado Com Sucesso!");
         } else if (JSON.parse(request.responseText) === false && API === 'api') {
           alert("Registrado Com Sucesso!");
