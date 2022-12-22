0770372def780a708a24bf85726ebc285a3a2992
henriquemtss <henriquegomesdematos08@gmail.com>
2022-12-22 14:23:19 -0300

    Projeto versão 1.0 Finalizado

--- controller/CadastroFuncionarioController.php
+++ controller/CadastroFuncionarioController.php
@@ -1,37 +1,26 @@
 <?php
-    include_once('../model/CadastroFuncionario.php');
-    include_once('../assets/lib/ValidadorCPF/ValidadorCpf.php');
-
-    if (!isset($_SESSION)) {
-        session_start();
-    }
-
-
-
-    
-
-    $cadastro = new CadastroFuncionario($_GET['nomeFuncionario'], $_GET['cpfFuncionario'], $_GET['emailFuncionario'], $_GET['telefoneFuncionario'], $_GET['funcaoFuncionario']);
-
-
-
-    if ($cadastro->verificarCampos()) {
-
-        if ($cadastro->verificarEmailECPF()) {
-             $cadastro->Cadastrar();
-        }
-    
-
-    }
-    
-       
-
-
-
-
-
-        
-
-        
-
-
+     include_once('../model/CadastroFuncionario.php');
+     include_once('../assets/lib/ValidadorCPF/ValidadorCpf.php');
+ 
+     if (!isset($_SESSION)) {
+         session_start();
+     }
+ 
+     $cpfReviewed = str_replace(".","",$_GET['cpfFuncionario']);
+     $cpfReviewed = str_replace("-","",$cpfReviewed);
+ 
+     
+ 
+     $cadastro = new CadastroFuncionario($_GET['nomeFuncionario'], $cpfReviewed, $_GET['emailFuncionario'], $_GET['telefoneFuncionario'], $_GET['funcaoFuncionario']);
+ 
+ 
+ 
+     if ($cadastro->verificarCampos()) {
+ 
+         if ($cadastro->verificarEmailECPF()) {
+              $cadastro->Cadastrar();
+         }
+     
+ 
+     }
 ?>
